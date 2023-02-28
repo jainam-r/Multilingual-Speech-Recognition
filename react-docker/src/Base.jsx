4f310cd9bb8fdea2f153bda3310fa016644e71a3
jainam <jainam.rambhiar@gmail.com>
2023-02-28 15:43:30 +0530

    first commit

---
+++ react-docker/src/Base.jsx
@@ -0,0 +1,11 @@
+// import 'react' from React;
+export const Base = () => {
+    
+    return(
+        <div>
+            <h3>
+                React app
+            </h3>
+        </div>
+    )
+}
